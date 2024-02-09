1ca3d31f85fe355085ed66de9db5804ccab3e015
Hidekazu Kobayashi <koba789@arkedgespace.com>
2024-02-09 15:48:07 +0000

    Use corepack and pnpm

--- tmtc-c2a/devtools_frontend/vite.config.ts
+++ tmtc-c2a/devtools_frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 export default defineConfig({
   resolve: {},
   base: "/devtools/",
-  plugins: [react(), pluginRewriteAll()],
+  plugins: [react()],
   server: {
     hmr: {},
   },
