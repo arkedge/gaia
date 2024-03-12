0e3a90150903d621debb9f36eab83facde13b0fc
sksat <sksat@arkedgespace.com>
2024-03-12 21:11:55 +0900

    remove unused import

--- tmtc-c2a/build.rs
+++ tmtc-c2a/build.rs
@@ -1,8 +1,4 @@
-use std::process::Command;
-use std::{
-    env, fs, io,
-    path::{Path, PathBuf},
-};
+use std::{env, path::PathBuf};
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
